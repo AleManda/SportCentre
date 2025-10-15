eddf2a7a69ed3419b6e9dcc1dd6a2d33a8a36c0b
alessandro.mandarini <a.mandarini@fastcode.it>
2025-10-15 02:36:00 +0200

    14ott pc2

--- SportCentre/Pages/AttivitaSportive/PrenotazioniIndex.cshtml.cs
+++ SportCentre/Pages/AttivitaSportive/PrenotazioniIndex.cshtml.cs
@@ -14,6 +14,11 @@ namespace SportCentre.Pages.AttivitaSportive
     {
         private readonly SportCentre.Data.ApplicationDbContext _context;
 
+        [BindProperty]
+        public string CurrentFilterDate { get; set; }
+        public string CurrentFilterAttivita { get; set; }
+        public string CurrentFilterUser { get; set; }
+
         public PrenotazioniIndexModel(SportCentre.Data.ApplicationDbContext context)
         {
             _context = context;
@@ -21,11 +26,29 @@ namespace SportCentre.Pages.AttivitaSportive
 
         public IList<Prenotazione> Prenotazioni { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string searchdate,string searchuser,string searchattivita)
         {
-            Prenotazioni = await _context.prenotazioni
+            IQueryable<Prenotazione> prenotazioniIQ = _context.prenotazioni
                 .Include(p => p.Attivita)
-                .Include(p => p.User).ToListAsync();
+                .Include(p => p.User);
+
+            if(!string.IsNullOrEmpty(searchdate) && DateOnly.TryParse(searchdate, out var parsedDate))
+            {
+                prenotazioniIQ = prenotazioniIQ.Where(p => p.Data == parsedDate);
+            }
+            if (!string.IsNullOrEmpty(searchuser))
+            {
+                prenotazioniIQ = prenotazioniIQ.Where(p => p.User.UserName.Contains(searchuser));
+            }
+            if (!string.IsNullOrEmpty(searchattivita))
+            {
+                prenotazioniIQ = prenotazioniIQ.Where(p => p.Attivita.Name.Contains(searchattivita));
+            }
+
+            Prenotazioni = await prenotazioniIQ.ToListAsync();
+            //Prenotazioni = await _context.prenotazioni
+            //    .Include(p => p.Attivita)
+            //    .Include(p => p.User).ToListAsync();
         }
     }
 }
---
+++ SportCentre/Pages/AttivitaSportive/PrenotazioniUserIndex.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SportCentre.Data;
+using SportCentre.Models;
+
+namespace SportCentre.Pages.AttivitaSportive
+{
+    public class PrenotazioniUserIndexModel : PageModel
+    {
+        private readonly SportCentre.Data.ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public PrenotazioniUserIndexModel(SportCentre.Data.ApplicationDbContext context,UserManager<IdentityUser> usermanager)
+        {
+            _context = context;
+            _userManager = usermanager;
+        }
+
+        public IList<Prenotazione> Prenotazioni { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null) {
+                return;
+            }
+
+            Prenotazioni = await _context.prenotazioni
+                .Include(p => p.Attivita)
+                .Include(p => p.User).Where(p => p.User.Id == user.Id).ToListAsync();
+        }
+    }
+}
